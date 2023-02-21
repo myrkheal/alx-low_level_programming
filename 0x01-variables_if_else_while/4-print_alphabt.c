b7042e740e910178ec26ad2999d961e6f313dc12
myrkheal <miotad12@gmail.com>
2023-02-20 16:17:57 -0800

    letter except

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: all letters except q and e
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
